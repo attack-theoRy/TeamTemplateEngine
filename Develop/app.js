bac68cdf4d3d22dfd225099cbf6aa022ad340020
Pirooz Wallace <PiroozWallace@outlook.com>
2020-12-07 17:23:18 -0800

    installing proper dependencies
    forgot to install package within develop

--- Develop/app.js
+++ Develop/app.js
@@ -33,3 +33,5 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
